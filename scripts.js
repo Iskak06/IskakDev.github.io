32562a8d0daaf0809746dc0665f9719edbc38e5b
Iskak06 <82656149+Iskak06@users.noreply.github.com>
2024-04-21 17:34:27 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+// Close menu when a list item is pressed
+const listItems = document.querySelectorAll(".menu li");
+listItems.forEach(item => {
+  item.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+});
+
+
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) =>{
+//         console.log(entry);
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         }
+//         else{
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+
+// const hiddenElements = document.querySelectorAll('.hidden, .hidden2');
+// hiddenElements.forEach((element) => observer.observe(element));
